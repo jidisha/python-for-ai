15c635febfcb19d8477dd1877e4acb37152694b0
jidisha <jidishasijapati@gmail.com>
2026-09-16 21:03:58 +0545

    Using package and using Jupyter command

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
-print("Hello world!")
-print("This is a simple Python script that prints 'Hello world!' to the console.")
+import requests
+
+# Download a web page
+response = requests.get("https://api.github.com")
+print(response.status_code)  # Should print 200
