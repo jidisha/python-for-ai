942d7e66ee1b14da0c54bb25eb8c18e188df4ae7
jidisha <jidishasijapati@gmail.com>
2026-09-22 21:38:50 +0545

    added requirements.txt

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-import requests
+# import requests
 
-# Download a web page
-response = requests.get("https://api.github.com")
-print(response.status_code)  # Should print 200
+# # Download a web page
+# response = requests.get("https://api.github.com")
+# print(response.status_code)  # Should print 200
+
+def greet():
+    print("Hello, World!" )
+    print("Hello, Again!" )
+
+greet()    
